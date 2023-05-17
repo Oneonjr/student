390a2c86ace2ed5a118bcab7fc86efe5387952b3
Oneonjr <oneonjunior@outlook.com>
2023-05-16 21:36:50 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'student';
+  students: { type: string; name: string; description: string; }[] = [];
+  newStudentName = '';
+  newStudentDescription = '';
+
+  
+  addApproved() {
+    this.students.push({
+      type:  'approved',
+      name:  this.newStudentName,
+      description: this.newStudentDescription
+    });
+
+  }
+
+  addSubstitute(){
+    this.students.push({
+      type: 'substitute',
+      name: this.newStudentName,
+      description: this.newStudentDescription
+    });
+  }
 }
